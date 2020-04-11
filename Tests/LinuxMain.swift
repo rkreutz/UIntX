06c0d9274acdeaef3fbfe2da526f87e30c2287b4
Rodrigo Kreutz <rkreutz>
2020-04-11 21:37:37 +0100

    Trying to fix Linux tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,13 +1,5 @@
 import XCTest
 
-@testable import UIntXTests
+import UIntXTests
 
-var tests = [XCTestCaseEntry]()
-tests += UIntXComparableTests.allTests
-tests += UIntXDescription.allTests
-tests += UIntXHash.allTests
-tests += UIntXInitTests.allTests
-tests += UIntXLeftShiftTests.allTests
-tests += UIntXOperations.allTests
-tests += UIntXRightShiftTests.allTests
-XCTMain(tests)
+XCTMain(allTests())
