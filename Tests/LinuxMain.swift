f0f6d4860f13ddee3468dc7f54b24ae3a02f1690
Rodrigo Kreutz <rkreutz>
2020-04-11 21:26:06 +0100

    Trying to fix Linux CI workflow

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,11 +3,11 @@ import XCTest
 @testable import UIntXTests
 
 var tests = [XCTestCaseEntry]()
-tests += UIntXComparableTests.allTests()
-tests += UIntXDescription.allTests()
-tests += UIntXHash.allTests()
-tests += UIntXInitTests.allTests()
-tests += UIntXLeftShiftTests.allTests()
-tests += UIntXOperations.allTests()
-tests += UIntXRightShiftTests.allTests()
+tests += UIntXComparableTests.allTests
+tests += UIntXDescription.allTests
+tests += UIntXHash.allTests
+tests += UIntXInitTests.allTests
+tests += UIntXLeftShiftTests.allTests
+tests += UIntXOperations.allTests
+tests += UIntXRightShiftTests.allTests
 XCTMain(tests)
